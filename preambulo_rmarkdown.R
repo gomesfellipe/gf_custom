c5d15f5129c9a3f2c3c9805462068ca68e2c2895
Fellipe <gomes.fellipe@hotmail.com>
2018-11-25 21:28:36 -0200

    Update preambulo_rmarkdown.R

--- preambulo_rmarkdown.R
+++ preambulo_rmarkdown.R
@@ -1,10 +1,9 @@
 # Opcoes do RMarkdown
 knitr::opts_chunk$set(echo = TRUE, warning = F, fig.height = 7,
                       fig.width = 10, knitr.kable.NA = '',OutDec = ",")
-
 options(scipen = 99999)
 # Carregando pacotes
-packages <- c("purrr", "dplyr", "ggplot2")
+packages <- c("purrr", "dplyr", "ggplot2", "knitr", "kableExtra")
 suppressMessages(purrr::walk(packages,library, character.only = TRUE, warn.conflicts = FALSE))
 rm(packages)
 
@@ -16,9 +15,20 @@ source("https://raw.githubusercontent.com/gomesfellipe/functions/master/grafico_
 theme_set(theme_bw())
 
 # Tema das tabelas
-kable2 <- function(x,...){
-  kable(x,digits = 4,...) %>%
+kable2 <- function(x,linhas=NULL,colunas=NULL, ...){
+  k <- 
+    kable(x,digits = 4,...) %>%
     kable_styling(bootstrap_options = "striped", full_width = F) %>%
-    kable_styling(c("striped", "bordered")) %>%
-    scroll_box(width = "100%")
+    kable_styling(c("striped", "bordered")) 
+  if (!is.null(linhas)) {
+    # destque na linha:
+    k <-  k %>% row_spec(linhas, bold = T, color = "white", background = "#CCE2FF")
+  }
+  
+  if (!is.null(colunas)) {
+    # destque na colunas:
+    k <-  k %>% column_spec(colunas,bold=T, color="white", background = "#CCE2FF")
+  }
+  k
 }
+
